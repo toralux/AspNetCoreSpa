aa7a3d50fac3329fbb6bff55a9283b8554298411
Asad Sahi <asadazmat@gmail.com>
2017-01-16 23:36:53 +0000

    package upgrades

--- Client/app/+examples/examples-home/examples-home.component.ts
+++ Client/app/+examples/examples-home/examples-home.component.ts
@@ -16,7 +16,7 @@ export class ExamplesHomeComponent implements OnInit {
       { route: 'advanceddirectives', title: 'Advanced directives', description: 'Advanced directives examples' },
       { route: 'components', title: 'Component examples', description: 'Various component examples' },
       { route: 'jqueryintegration', title: 'jQuery integration', description: 'Implementation of jquery inside Angular 2' },
-      { route: 'changedetection', title: 'Change detection', description: 'Default vs onPush to illustrate better performance' }
+      { route: 'changedetection', title: 'Change detection', description: 'Default vs onPush to illustrate better performance' },
       { route: 'reactiveforms', title: 'Reactive forms', description: 'Examples of reactive forms using angular reactive forms module' }
     ];
   }
