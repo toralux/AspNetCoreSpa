15ea587897b314a0a8dc5b6d0998605dffba0cbb
Tor Anders Johansen <mail@toranders.com>
2017-08-08 19:03:01 +0200

    Finally removed all social login dependencies

--- Client/app/+login/login.module.ts
+++ Client/app/+login/login.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
-import { SocialLoginComponent } from './sociallogin/social-login.component';
 import { LoginComponent } from './login.component';
 import { routing } from './login.routes';
 
 @NgModule({
     imports: [routing, SharedModule],
-    declarations: [LoginComponent, SocialLoginComponent]
+    declarations: [LoginComponent]
 })
 export class LoginModule { }
